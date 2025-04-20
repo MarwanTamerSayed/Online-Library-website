1bb1f8a0f166d77582fcad44d574a3cb353e4fd6
MarwanTamerSayed <20230380@stud.fci-cu.edu.eg>
2025-04-20 15:33:37 +0200

    Update BorrowedBooks.js

--- Marwa/BorrowedBooks.js
+++ Marwa/BorrowedBooks.js
@@ -29,4 +29,19 @@ console.log("Current Date:", currentDate);
 window.onload = function() {
   overdue();  
 }; 
- 
+// Search function 
+function searchBorrowedBooks() {
+  const input = document.getElementById('searchInput').value.toLowerCase();
+  const bookItems = document.querySelectorAll('.book');
+
+  bookItems.forEach(book => {
+      const title = book.querySelector('h3').textContent.toLowerCase();
+      const author = book.querySelector('.info p:nth-of-type(1)').textContent.toLowerCase();
+
+      if (title.includes(input) || author.includes(input)) {
+          book.style.display = 'block';
+      } else {
+          book.style.display = 'none';
+      }
+  });
+}
